80859bd8ff0f4b056a4f4dc3827bd6a36445c577
thanhnt <thanhntggsunday@gmail.com>
2025-06-23 22:49:52 +0700

    coding for pms

---
+++ Blogifier.Core/Modules/Pms/Models/Dto/CartDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blogifier.Core.Entities;
+
+namespace Blogifier.Core.Modules.Pms.Models.Dto
+{
+    public class CartDto
+    {
+        public string UserName { get; set; }
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
+    }
+}
