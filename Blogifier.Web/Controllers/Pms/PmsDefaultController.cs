d9604d1d11c47c462407a80c09d516185d6b8244
thanhnt <thanhntggsunday@gmail.com>
2025-07-07 20:28:59 +0700

    Add Pms DI

--- Blogifier.Web/Controllers/Pms/PmsDefaultController.cs
+++ Blogifier.Web/Controllers/Pms/PmsDefaultController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Blogifier.Core.Common;
+using Blogifier.Core.Modules.Pms.Interfaces;
+using Blogifier.Core.Modules.Pms.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blogifier.Web.Controllers
@@ -10,8 +12,23 @@ namespace Blogifier.Web.Controllers
     [Route("[controller]/[action]")]
     public class PmsDefaultController : Controller
     {
-        public IActionResult Index()
+
+        private ICartProvider _cartProvider;
+        private IOrderProvider _orderProvider;
+        private IProvider<ProductCategoryDto> _productCategoryProvider;
+        private IProvider<ProductDto> _productProvider;
+        private IProvider<TagDto> _tagProvider;
+
+        public IActionResult Index(ICartProvider cartProvider, IOrderProvider orderProvider,
+            IProvider<ProductCategoryDto> productCategoryProvider, IProvider<ProductDto> productProvider,
+            IProvider<TagDto> tagProvider)
         {
+            _productCategoryProvider = productCategoryProvider;
+            _productProvider = productProvider;
+            _cartProvider = cartProvider;
+            _orderProvider = orderProvider;
+            _tagProvider = tagProvider;
+
             return View($"~/{ApplicationSettings.BlogThemesFolder}/OneFour/Default.cshtml");
         }
     }
